8320a7308bed0c531ce8b213c05c380a36f4653d
Bhargavi-Savani <76099040+Bhargavi-Savani@users.noreply.github.com>
2022-04-15 00:44:27 +0530

    Add files via upload

---
+++ P9.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, rollno, name, division):
+        self.name = name
+        self.rollno = rollno
+        self.divison = division
+
+    def getStudent(self):
+        print("Name: ", self.name)
+        print("Enrollment No: ", self.rollno)
+        print("Division: ", self.divison)
+
+
+class Exam(Student):
+    def setMarks(self, marks):
+        self.marks = marks
+
+    def getMarks(self):
+        return self.marks
+
+
+class Result(Exam):
+    def getTotalMarks(self):
+        Total_Marks = sum(self.getMarks())
+        return Total_Marks
+
+
+object = Result(75, "Bhargavi Savani", 2)
+print("\nStudent Information: ")
+object.getStudent()
+object.setMarks([77, 69, 83, 71, 70, 91])
+print("Total Marks:", object.getTotalMarks())
